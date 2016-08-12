62e811051d0a8ce2db49b456aef08c9347a9c711
Jon Wheeler <jon@doejo.com>
2016-08-11 22:27:30 -0400

    Remove test css.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import "./app.css";
-import styles from "./styles.module.css";
-
 const App = React.createClass({
   render: function() {
     return (
-      <div className = { styles.wrapper }>
-        Text text text
+      <div>
+        <h1>Environment: { __NODE_ENV__ }</h1>
       </div>
     );
   }
